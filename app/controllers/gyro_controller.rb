97a1183885c5c82b155f4b6844238cf9ef0b49c8
slacker87 <racethesunlive@yahoo.com>
2015-03-03 04:32:58 +0000

    gyro 3d working

--- app/controllers/gyro_controller.rb
+++ app/controllers/gyro_controller.rb
@@ -1,4 +1,8 @@
 class GyroController < ApplicationController
   def index
+    gyrofile = Tail.new('./streamed_data/gyro.txt').tail(1).flatten
+    @gyrotime = gyrofile.first
+    @gyrodata = gyrofile.second.gsub('euler','')[3..-1].chomp.split('   ')
+    unless @gyrodata.present? then @gyrodata = "N/A" end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
     @latestimage = Image.new("./streamed_images/").latest.flatten
+    #for gyro cube
+      gyrofile = Tail.new('./streamed_data/gyro.txt').tail(1).flatten
+      @gyrotime = gyrofile.first
+      @gyrodata = gyrofile.second.gsub('euler','')[3..-1].chomp.split('   ')
+      unless @gyrodata.present? then @gyrodata = "N/A" end
+    #end gyro cube
   end
 
   def about
