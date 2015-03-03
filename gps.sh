1c277f19ad63e44d94d7a8d608d66a838ca0b4c3
Mike <ccnp_pro@yahoo.com>
2015-03-03 02:44:29 +0000

    gps script working properly now

--- gps.sh
+++ gps.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gpspipe -w -o gps.txt -t -d
+killall gpsd
+sleep 2
+touch ./streamed_data/gps.txt
+gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock
+sleep 2
+gpspipe -w -l -t -o ./streamed_data/gps.txt &
